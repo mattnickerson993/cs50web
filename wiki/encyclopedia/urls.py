114d37d12b93042b03fe32f113424d73dccafa76
Matt Nickerson <mattnickerson@Matts-MacBook-Pro.local>
2020-12-18 08:21:18 -0500

    updated index links

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+
+
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<title>", views.display_page, name="display_page")
+    path("<str:title>", views.display_page, name="display_page")
 ]
